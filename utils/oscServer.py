50717c8d7c805fb318cf0a3fdc3f567c0a702a9f
Axel Chemla-Romeu-Santos <axel.chemla-romeu-santos@ircam.fr>
2020-02-25 13:56:19 +0100

    offline playground

--- utils/oscServer.py
+++ utils/oscServer.py
@@ -6,13 +6,20 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 import torch, numpy as np
 from librosa import stft, istft
+from librosa.output import write_wav
 from scipy import fft, ifft
+from scipy.signal import resample
 from time import  process_time
+from ..data.signal.transforms import computeTransform, inverseTransform
 from ..vaes import AbstractVAE
+from . import checklist, checktuple
 from ..monitor import visualize_dimred as dr
-import dill
+import dill, random, string
 
 from ..vaes import *
+
+SESSION_ID = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+SESSION_HISTORY_LENGTH = 10
 
 
 # phase reconstruction
@@ -47,6 +54,7 @@ def osc_attr(obj, attribute):
             return setattr(obj, attribute, *args)
     return closure
 
+
 class OSCServer(object):
     '''
     Key class for OSCServers linking Python and Max / MSP
@@ -75,12 +83,12 @@ class OSCServer(object):
     def init_bindings(self, osc_attributes=[]):
         '''Here we define every OSC callbacks'''
         self.dispatcher.map("/ping", self.ping)
-        self.dispatcher.map("/stop", self.stopServer)
+        self.dispatcher.map("/stop", self.stop_server)
         for attribute in osc_attributes:
             print(attribute)
             self.dispatcher.map("/%s"%attribute, osc_attr(self, attribute))
 
-    def stopServer(self, *args):
+    def stop_server(self, *args):
         '''stops the server'''
         self.client.send_message("/terminated", "bang")
         self.server.shutdown()
@@ -98,6 +106,20 @@ class OSCServer(object):
     def send(self, address, content):
         '''global method to send a message'''
         self.client.send_message(address, content)
+
+    def send_bach_series(self, message, dim, series):
+        out = ['series']
+        if (message == '/encode'):
+            out.append(dim + 1)
+            str_out = "(" + str(dim + 1)
+        else:
+            str_out = "(" + dim
+        for s2 in range(series.shape[0]):
+            str_out += ' ( ' + str(float(s2) / series.shape[0]) + ' ' + str(series[s2]) + ' 0. )'
+        str_out += ' )'
+        out.append(str_out)
+        # print(str_out)
+        self.send(message, out)
 
     def print(self, *args):
         print(*args)
@@ -147,7 +169,7 @@ def dict2str(dic):
     return str[2:]
 
 class VAEServer(OSCServer):
-    osc_attributes = ['model', 'projection']
+    osc_attributes = ['model', 'projection', 'current_layer']
     min_threshold = 0
     # model attributes
     def getmodel(self):
@@ -155,26 +177,35 @@ class VAEServer(OSCServer):
     def delmodel(self): del self._model
     def setmodel(self, *args):
        self._model_path = None
-       if len(args) == 1:
-           if type(args[0]) == str:
-               self.print('loading model %s...'%args[0])
-               try:
+       try:
+           if len(args) == 1:
+               if args[0] is None:
+                   return
+               if type(args[0]) == str:
+                   self.print('Loading model %s...'%args[0])
                    loaded_data = torch.load(args[0], map_location='cpu')
-               except FileNotFoundError:
-                   self.print('file %s not found'%args[0])
-                   pass
 
-               self._model = loaded_data['class'].load(loaded_data)
-               self._model_path = args[0]
-               if 'preprocessing' in loaded_data.keys():
-                   self.preprocessing = loaded_data['preprocessing']
-                   print(self.preprocessing)
-       elif issubclass(type(args[0]), AbstractVAE):
-           self._model = args[0]
-       self.print('model loaded')
-       self.send('/model_loaded', 'bang')
-       self.current_projection = None
-       self.get_state()
+                   self._model = loaded_data['class'].load(loaded_data)
+                   self.transformOptions = loaded_data.get('transformOptions')
+                   if self.transformOptions.get('resampleTo'):
+                       self.sr = self.transformOptions['resampleTo']
+                   self._model_path = args[0]
+                   if 'preprocessing' in loaded_data.keys():
+                       self.preprocessing = loaded_data['preprocessing']
+           elif issubclass(type(args[0]), AbstractVAE):
+               self._model = args[0]
+           self.print('model loaded')
+           self.send('/model_loaded', 'bang')
+           self.current_projection = None
+           self.get_state()
+           if self._model:
+               self._model.eval()
+           self.print('Server ready')
+       except Exception as e:
+           self.print('Loading failed')
+           print('-- Loading model failed')
+           print(e)
+
     model = property(getmodel, setmodel, delmodel, "vae model attached to server")
 
     # projection attributes
@@ -196,63 +227,253 @@ class VAEServer(OSCServer):
     projection = property(getprojection, setprojection, delprojection)
 
 
-    def getselector(self): return self._selector
-    def delselector(self): raise Exception("VAEServer's cannot be deleted")
-    def setselector(self, *args):
-        if issubclass(type(args[0]), str):
-            self._selector = self.selector_hash[args[0]]
-        else:
-            self._selector = args[0]
-    selector = property(getselector, delselector, setselector)
-
+    def getlayer(self): return self._current_layer
+    def dellayer(self): raise Exception("attribute current_layer cannot be deleted")
+    def setlayer(self, *args):
+        try:
+            self._current_layer = int(args[0])
+        except TypeError:
+            print('current_layer attribute must be parsed as int')
+    current_layer = property(getlayer, setlayer, dellayer)
 
     def __init__(self, *args, **kwargs):
-        self._model = kwargs.get('model')
-        self._model_path = None
-        self._projections = {}
+        self._model = None
+        self.model = kwargs.get('model')
+        self.model_path = None
+        self.projections = {}
         self.current_projection = None
+        self.current_gen_id = 0
+
+        self.condition_params = None
+        self.current_phase = None
         self.phase_reconstruction = None
+
         self.dataset = kwargs.get('dataset')
+        self.transformOptions = kwargs.get('transformOptions')
         self.preprocessing = kwargs.get('preprocessing')
+        self.sr = 22050
+
+        # navigation option
+        self._current_layer = 0
+        self.current_traj = None
         self.phase_callback = random_static
+        self.current_conditioning = {}
         self._selector = selector_all(self.dataset) if self.dataset is not None else None
 
         super(VAEServer, self).__init__(*args)
         self.get_state()
+        self.print('Server ready.')
 
     def init_bindings(self, osc_attributes=[]):
         super(VAEServer, self).init_bindings(self.osc_attributes)
+
+        # Offline methods
+        self.dispatcher.map('/get_state', osc_parse(self.get_state))
+        self.dispatcher.map('/resend_state', osc_parse(self.resend_state))
+        # Change option flags
+        # self.dispatcher.map('/set_freeze_mode', osc_parse(self.set_freeze_mode))
+        # self.dispatcher.map('/set_scaling', osc_parse(self.set_scaling))
+        # self.dispatcher.map('/set_path_use_z', osc_parse(self.set_path_use_z))
+        # self.dispatcher.map('/set_interpolation_mode', osc_parse(self.set_interpolation_mode))
+        # # Changing model and dataset
+        # self.dispatcher.map('/select_model', osc_parse(self.select_model))
+        # self.dispatcher.map('/set_model', osc_parse(self.set_model))
+        # self.dispatcher.map('/set_dataset', osc_parse(self.set_dataset))
+        # # Latent path operations
+        # self.dispatcher.map('/clear_path', osc_parse(self.clear_path))
+        # self.dispatcher.map('/add_free_path', osc_parse(self.add_free_path))
+        # self.dispatcher.map('/add_preset_path', osc_parse(self.add_preset_path))
+        # self.dispatcher.map('/play_path', osc_parse(self.play_path))
+        # # Core functionalities (load, decode, encode)
+        self.dispatcher.map('/condition', osc_parse(self.condition))
+        self.dispatcher.map('/encode', osc_parse(self.encode))
+        self.dispatcher.map('/decode', osc_parse(self.decode))
+        # self.dispatcher.map('/interpolate', osc_parse(self.interpolate))
+
+        # Online methods
         self.dispatcher.map('/get_spectrogram', osc_parse(self.get_spectrogram))
         self.dispatcher.map('/add_projection', osc_parse(self.add_projection))
         self.dispatcher.map('/load', osc_parse(self.load_projections))
         self.dispatcher.map('/save', osc_parse(self.save))
         self.dispatcher.map('/get_projections', osc_parse(self.get_projections))
-        self.dispatcher.map('/get_state', osc_parse(self.get_state))
         self.dispatcher.map('/add_anchor', osc_parse(self.add_anchor))
         self.dispatcher.map('/load_anchor', osc_parse(self.load_anchor))
 
 
-    def get_state(self):
-        if self.current_projection is not None:
-            input_dim = self.projection.dim
-        elif self.model is not None:
-            input_dim = self._model.pinput['dim']
-        else:
-            input_dim = 0
-        state = {'current_projection': str(self.current_projection),
-                 'projections': list(self.model.manifolds.keys()),
-                 'input_dim': input_dim}
 
-        state['anchors'] = list(self.model.manifolds[self.current_projection].anchors.keys())
+    def resend_state(self):
+        self.get_state()
+        self.print('Server is ready.')
+
+    def get_condition_list(self):
+        if self.model is None:
+            return "None"
+        hidden_params = []
+        for ph in checklist(self.model.phidden):
+            if ph.get('encoder') or ph.get('decoder'):
+                if ph.get('encoder'):
+                    hidden_params.append(ph['encoder'])
+                if ph.get('decoder'):
+                    hidden_params.append(ph['decoder'])
+            else:
+                hidden_params.extend(ph)
+
+        labels = {}
+        for ph in hidden_params:
+            if ph.get('label_params') is not None:
+                labels = {**labels, **ph['label_params']}
+
+        conds = [[k, 'intlist', 0, v['dim']] for k, v in labels.items()]
+
+        return conds, labels
+
+    def get_state(self):
+        conditions_list = "None"; conditions_names = "None"; input_dim = 0; latent_dim = 0; n_layers = 0
+        if self.model is None:
+            state = {}
+        else:
+            input_dim = self.model.pinput['dim']
+            if self.current_projection is not None:
+                latent_dim = self.projection.dim
+            elif self.model is not None:
+                latent_dim = self.model.platent[self.current_layer]['dim']
+
+            conditions_list, self.condition_params = self.get_condition_list()
+            conditions_names = [c[0] for c in conditions_list] if len(conditions_list) > 0 else ["None"]
+            conditions_list = sum(conditions_list, [])
+            n_layers = len(self.model.platent)
+            state = {'current_projection': str(self.current_projection),
+                     'projections': list(self.model.manifolds.keys()),
+                     'conditioning': conditions_names,
+                     'input_dim': input_dim, 'latent_dim':latent_dim}
+
+            state['anchors'] = list(self.model.manifolds[self.current_projection].anchors.keys())
 
         state_str = dict2str(state)
-        self.send('/state', state_str)
+        if len(state_str) != 0:
+            self.send('/state', state_str)
+
+        self.send('/conditions_list', conditions_list if len(conditions_list) > 0 else "None")
+        self.send('/conditions_names', conditions_names)
+        self.send('/latent_dims', latent_dim)
+        self.send('/num_layers', n_layers)
         return state
+
+
+    def encode(self, *args):
+        try:
+            assert len(args) == 1, "encode function needs at least one argument"
+            self.print('Computing transform...')
+            if type(args[0]) == str:
+                file_path = args[0]
+                assert self.transformOptions, "direct encoding of files need transform options"
+                transform = computeTransform([file_path], "stft", options=self.transformOptions)[0]
+            elif type(args[0]) == np.ndarray or torch.is_tensor(args[0]):
+                transform = args[0]
+
+            self.print('Preprocessing...')
+            if self.preprocessing:
+                self.current_phase = np.angle(transform)
+                transform = self.preprocessing(transform)
+
+
+            y = {}
+            if self.condition_params != {}:
+                for k, t in self.condition_params.items():
+                    if self.current_conditioning.get(k) is not None:
+                        if len(self.current_conditioning[k]) > 2:
+                            y[k] = torch.from_numpy(resample(np.array(self.current_conditioning[k]), transform.shape[0]))
+                        else:
+                            y[k] = torch.from_numpy(self.current_conditioning[k].repeat(transform.shape[0]))
+                    else:
+                        y[k] = torch.zeros(transform.shape[0]).int()
+
+            self.print('Encoding...')
+            with torch.no_grad():
+                vae_out = self.model.encode(self.model.format_input_data(transform), y=y)
+            latent = vae_out[self.current_layer]['out_params'].mean.cpu().numpy().T
+
+            if (len(latent.shape) == 1):
+                self.send('/encode', ['point', latent])
+            else:
+                # PAD LATENT SERIE SO THAT IT'S A POWER OF 2
+                # N = 2**np.ceil(np.log2(latent.shape[-1])).astype(int)
+                # latent = np.pad(latent, ((0,0),(0,N - latent.shape[-1])), "constant")
+                #resamp_latent = self.latent_sd.add_original(latent, 65)
+
+                # print(f"resamp_latent.shape={resamp_latent.shape}")
+                # if condition is not None:
+                #     for k, v in condition.items():
+                #         v = np.pad(v, (0,N - v.shape[-1]))
+                #         resamp_v = self.cdt_sd[k].add_original(v, 65)
+                #         self.send_bach_series('/condition', k, resamp_v)
+                #         self._model.cond_vals[k] = v
+
+                for s in range(latent.shape[0]):
+                    resamp = latent[s]
+                    self.send_bach_series('/encode', s, resamp)
+            self.current_traj = latent
+            self.print('Server ready')
+        except Exception as e:
+            self.print('Encoding failed.')
+            print('--Encoding failed.')
+            print(e)
+
+    def decode(self, *args):
+        if args[0] == "series":
+            input_args = args[1].split(' ')
+            dim_id = int(input_args[0]) - 1;  dim_traj = np.array([float(a) for a in input_args[1:]])
+            self.current_traj[dim_id] = resample(dim_traj, self.current_traj.shape[1])
+
+        if args[0] == "generate":
+            try:
+                y = {}
+                if self.condition_params != {}:
+                    for k, t in self.condition_params.items():
+                        if self.current_conditioning.get(k) is not None:
+                            if len(self.current_conditioning[k]) > 2:
+                                y[k] = torch.from_numpy(resample(np.array(self.current_conditioning[k]), self.current_traj.shape[1]))
+                            else:
+                                y[k] = torch.from_numpy(self.current_conditioning[k].repeat(self.current_traj.shape[1]))
+                        else:
+                            y[k] = torch.zeros(self.current_traj.shape[1]).int()
+
+                self.print('Decoding...')
+                with torch.no_grad():
+                    vae_out = self.model.decode(self.model.format_input_data(self.current_traj.T), y=y)
+                reco = vae_out[self.current_layer]['out_params'].mean.cpu().numpy().T
+
+                self.print('Preprocessing...')
+                if self.preprocessing:
+                    reco = self.preprocessing.invert(reco)
+                if self.model.pinput['conv']:
+                    reco = reco.squeeze(-len(checktuple(self.model.pinput['dim']))-1)
+
+                assert self.transformOptions
+                if self.transformOptions:
+                    raw_data = inverseTransform(reco.T, 'stft', {'transformParameters':self.transformOptions}, originalPhase=self.current_phase)
+
+                name = '/tmp/%s_%d.wav'%(SESSION_ID, self.current_gen_id);
+                self.current_gen_id = (self.current_gen_id + 1)%SESSION_HISTORY_LENGTH
+                write_wav(name, raw_data, sr=self.sr)
+                self.send('/decode', name)
+                self.print('Server ready.')
+            except Exception as e:
+                self.print('Decoding failed.')
+                print('--Decoding failed.')
+                print(e)
+
+
+    def condition(self, *args):
+        cond_type = str(args[0])
+        cond_series = np.array([int(a) for a in args[1].split(' ')])
+        self.current_conditioning[cond_type] = cond_series
+
 
 
     def get_spectrogram(self, *args, projection=None, filter=True):
         if self._model is None:
-           print('[Error] spectrogram requested by not any model loaded')
+           print('d[Error] spectrogram requested by not any model loaded')
            self.send('/vae_error', '[Error] spectrogram requested by not any model loaded')
            return
 
