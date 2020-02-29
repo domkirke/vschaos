6ea3400a7d31a4ee2762087609276339037fa723
Axel Chemla-Romeu-Santos <axel.chemla-romeu-santos@ircam.fr>
2020-02-29 19:18:44 +0100

    offline playground, interpolation updates

--- utils/oscServer.py
+++ utils/oscServer.py
@@ -8,7 +8,7 @@ import torch, numpy as np
 from librosa import stft, istft
 from librosa.output import write_wav
 from scipy import fft, ifft
-from scipy.signal import resample
+from scipy.signal import resample, resample_poly
 from time import  process_time
 from ..data.signal.transforms import computeTransform, inverseTransform
 from ..vaes import AbstractVAE
@@ -20,7 +20,7 @@ from ..vaes import *
 
 SESSION_ID = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
 SESSION_HISTORY_LENGTH = 10
-
+MAX_TRAJECTORY_LENGTH = 64
 
 # phase reconstruction
 phase_rand = {}
@@ -67,9 +67,10 @@ class OSCServer(object):
     # attributes automatically bounded to OSC ports
     osc_attributes = []
     # Initialization method
-    def __init__(self, in_port, out_port, ip='127.0.0.1', *args):
+    def __init__(self, in_port, out_port, ip='127.0.0.1', verbose=True, *args):
         super(OSCServer, self).__init__()
         # OSC library objects
+        self.verbose = verbose
         self.dispatcher = dispatcher.Dispatcher()
         self.client = udp_client.SimpleUDPClient(ip, out_port)
         
@@ -122,8 +123,9 @@ class OSCServer(object):
         self.send(message, out)
 
     def print(self, *args):
-        print(*args)
-        self.send('/print', *args)
+        if self.verbose:
+            print(*args)
+            self.send('/print', *args)
 
 
 def selector_all(dataset):
@@ -198,6 +200,7 @@ class VAEServer(OSCServer):
            self.send('/model_loaded', 'bang')
            self.current_projection = None
            self.get_state()
+           self.init_interpolation()
            if self._model:
                self._model.eval()
            self.print('Server ready')
@@ -260,6 +263,9 @@ class VAEServer(OSCServer):
         self.current_conditioning = {}
         self._selector = selector_all(self.dataset) if self.dataset is not None else None
 
+        # Sets of latent points for interpolation
+        self.init_interpolation()
+        self.interpolate_latent = 1
         super(VAEServer, self).__init__(*args)
         self.get_state()
         self.print('Server ready.')
@@ -274,7 +280,6 @@ class VAEServer(OSCServer):
         # self.dispatcher.map('/set_freeze_mode', osc_parse(self.set_freeze_mode))
         # self.dispatcher.map('/set_scaling', osc_parse(self.set_scaling))
         # self.dispatcher.map('/set_path_use_z', osc_parse(self.set_path_use_z))
-        # self.dispatcher.map('/set_interpolation_mode', osc_parse(self.set_interpolation_mode))
         # # Changing model and dataset
         # self.dispatcher.map('/select_model', osc_parse(self.select_model))
         # self.dispatcher.map('/set_model', osc_parse(self.set_model))
@@ -288,9 +293,14 @@ class VAEServer(OSCServer):
         self.dispatcher.map('/condition', osc_parse(self.condition))
         self.dispatcher.map('/encode', osc_parse(self.encode))
         self.dispatcher.map('/decode', osc_parse(self.decode))
-        # self.dispatcher.map('/interpolate', osc_parse(self.interpolate))
-
+        self.dispatcher.map('/interpolate', osc_parse(self.interpolate))
+        self.dispatcher.map('/set_interpolation_mode', osc_parse(self.set_interpolation_mode))
         # Online methods
+
+
+        self.dispatcher.map('/encode_spectrogram', osc_parse(self.encode_spectrogram))
+        self.dispatcher.map('/decode_spectrogram', osc_parse(self.decode_spectrogram))
+
         self.dispatcher.map('/get_spectrogram', osc_parse(self.get_spectrogram))
         self.dispatcher.map('/add_projection', osc_parse(self.add_projection))
         self.dispatcher.map('/load', osc_parse(self.load_projections))
@@ -328,7 +338,7 @@ class VAEServer(OSCServer):
         return conds, labels
 
     def get_state(self):
-        conditions_list = "None"; conditions_names = "None"; input_dim = 0; latent_dim = 0; n_layers = 0
+        conditions_list = "None"; conditions_names = "None"; latent_dim = 0; n_layers = 0
         if self.model is None:
             state = {}
         else:
@@ -357,24 +367,30 @@ class VAEServer(OSCServer):
         self.send('/conditions_names', conditions_names)
         self.send('/latent_dims', latent_dim)
         self.send('/num_layers', n_layers)
+        self.send('/interpolation_list', list(self.interpolate_points.keys()))
         return state
 
 
-    def encode(self, *args):
+    def encode(self, *args, transform=True):
         try:
-            assert len(args) == 1, "encode function needs at least one argument"
-            self.print('Computing transform...')
-            if type(args[0]) == str:
-                file_path = args[0]
-                assert self.transformOptions, "direct encoding of files need transform options"
-                transform = computeTransform([file_path], "stft", options=self.transformOptions)[0]
-            elif type(args[0]) == np.ndarray or torch.is_tensor(args[0]):
-                transform = args[0]
+            if transform:
+                assert len(args) == 1, "if transform is True, encode function needs at least one argument"
+                self.print('Computing transform...')
+                if type(args[0]) == str:
+                    file_path = args[0]
+                    assert self.transformOptions, "direct encoding of files need transform options"
+                    transform = computeTransform([file_path], "stft", options=self.transformOptions)[0]
+                elif type(args[0]) == np.ndarray or torch.is_tensor(args[0]):
+                    transform = args[0]
 
-            self.print('Preprocessing...')
-            if self.preprocessing:
-                self.current_phase = np.angle(transform)
-                transform = self.preprocessing(transform)
+                if self.preprocessing:
+                    self.print('Preprocessing...')
+                    self.current_phase = np.angle(transform)
+                    transform = self.preprocessing(transform)
+            else:
+                transform = np.zeros((1, self.model.pinput['dim']))
+                transform[0, :len(args)] = np.array(args)
+                self.current_phase = np.zeros_like(transform)
 
 
             y = {}
@@ -387,11 +403,12 @@ class VAEServer(OSCServer):
                             y[k] = torch.from_numpy(self.current_conditioning[k].repeat(transform.shape[0]))
                     else:
                         y[k] = torch.zeros(transform.shape[0]).int()
-
-            self.print('Encoding...')
+            #
+            # self.print('Encoding...')
+            self.print('Forwarding...')
             with torch.no_grad():
                 vae_out = self.model.encode(self.model.format_input_data(transform), y=y)
-            latent = vae_out[self.current_layer]['out_params'].mean.cpu().numpy().T
+            latent = vae_out[self.current_layer]['out_params'].mean.cpu()
 
             if (len(latent.shape) == 1):
                 self.send('/encode', ['point', latent])
@@ -409,23 +426,38 @@ class VAEServer(OSCServer):
                 #         self.send_bach_series('/condition', k, resamp_v)
                 #         self._model.cond_vals[k] = v
 
+                if latent.shape[0] > MAX_TRAJECTORY_LENGTH:
+                    latent = self.get_deformed_serie(latent, target_length=MAX_TRAJECTORY_LENGTH, axis=0)
+
+                latent = latent.numpy().T
                 for s in range(latent.shape[0]):
-                    resamp = latent[s]
-                    self.send_bach_series('/encode', s, resamp)
+                    self.send_bach_series('/encode', s, latent[s])
             self.current_traj = latent
+            self.send('/latent', latent.squeeze().tolist())
             self.print('Server ready')
         except Exception as e:
             self.print('Encoding failed.')
             print('--Encoding failed.')
             print(e)
 
-    def decode(self, *args):
+    def encode_spectrogram(self, *args):
+       return self.encode(*args, transform=False)
+
+    def decode(self, *args, write=True, send_spectrogram=False):
         if args[0] == "series":
             input_args = args[1].split(' ')
             dim_id = int(input_args[0]) - 1;  dim_traj = np.array([float(a) for a in input_args[1:]])
             self.current_traj[dim_id] = resample(dim_traj, self.current_traj.shape[1])
 
-        if args[0] == "generate":
+        if args[0] == "point":
+            latent_dim = self.model.platent[self.current_layer]['dim']
+            self.current_traj = np.zeros((latent_dim,1))
+            if len(args)-1 >= latent_dim:
+                self.current_traj[:, 0] = np.array(args[1:latent_dim+1])
+            else:
+                self.current_traj[:len(args), 0] = np.array(args[1:])
+
+        if args[0] in ["generate", "point"]:
             try:
                 y = {}
                 if self.condition_params != {}:
@@ -449,20 +481,125 @@ class VAEServer(OSCServer):
                 if self.model.pinput['conv']:
                     reco = reco.squeeze(-len(checktuple(self.model.pinput['dim']))-1)
 
-                assert self.transformOptions
-                if self.transformOptions:
-                    raw_data = inverseTransform(reco.T, 'stft', {'transformParameters':self.transformOptions}, originalPhase=self.current_phase)
+                if write:
+                    assert self.transformOptions
+                    if self.transformOptions:
+                        raw_data = inverseTransform(reco.T, 'stft', {'transformParameters':self.transformOptions}, iterations=30)
+                    name = '/tmp/%s_%d.wav'%(SESSION_ID, self.current_gen_id);
+                    self.current_gen_id = (self.current_gen_id + 1)%SESSION_HISTORY_LENGTH
+                    write_wav(name, raw_data, sr=self.sr)
+                    self.send('/decode', name)
 
-                name = '/tmp/%s_%d.wav'%(SESSION_ID, self.current_gen_id);
-                self.current_gen_id = (self.current_gen_id + 1)%SESSION_HISTORY_LENGTH
-                write_wav(name, raw_data, sr=self.sr)
-                self.send('/decode', name)
+                if send_spectrogram:
+                    self.send('/spectrogram_out', reco.squeeze().tolist())
+
                 self.print('Server ready.')
             except Exception as e:
                 self.print('Decoding failed.')
-                print('--Decoding failed.')
+                # print('--Decoding failed.')
                 print(e)
 
+    def decode_spectrogram(self, *args):
+       return self.decode(*args, write=False, send_spectrogram=True)
+
+    # Interpolation methods
+
+    def init_interpolation(self):
+        # Reset dictionnary
+        self.interpolate_points = {}
+        self.interpolate_conditions = {}
+
+    def scale_interpolation(self):
+        for k, v in self.interpolate_points.items():
+            new_v = np.zeros((self._model.latent_dims, self.n_points * self.scaling))
+            for d in range(v.shape[0]):
+                new_v[d] = resample_poly(v[d], self.scaling * self.n_points, len(v[d]))
+            self.interpolate_points[k] = new_v
+
+    def get_deformed_serie(self, serie, target_serie=None, target_length=None, axis=0):
+        if target_serie:
+            target_length = target_serie.shape[0]
+        if target_length is None:
+            if self.current_traj is not None:
+                target_length = self.current_traj.shape
+            else:
+                return serie
+        if torch.is_tensor(serie):
+            serie = serie.numpy()
+        if len(serie) > 2:
+            serie = resample(np.array(serie), target_length, axis=axis)
+        else:
+            serie = np.array(serie).repeat(target_length, axis=axis)
+
+        return torch.from_numpy(serie)
+
+
+    def interpolate(self, val_list):
+        print(val_list)
+        self.print('Interpolating')
+        interp = val_list.split(' ')
+        if (interp[0] == 'current'):
+            print('Adding current point as ' + interp[1])
+            self.interpolate_points[interp[1]] = self.current_traj
+            if self.current_conditioning is not None:
+                self.interpolate_conditions[interp[1]] = {k:v.get_deformed_serie()
+                                                          for k,v in self.current_conditioning.items()}
+            print("Added !")
+            return
+        if (interp[0] == 'random'):
+            print('Adding random point as ' + interp[1])
+            self.interpolate_points[interp[1]] = np.random.randn(self._model.latent_dims, self.n_points * self.scaling)
+            return
+
+
+        interp_names = interp[::2]
+        interp_vals = [float(x) for x in interp[1::2]]
+        if (self.current_conditioning is not None):
+            for k, v in self.current_conditioning.items():
+                # Retrieve first point to infer size
+                f_point = self.interpolate_conditions[interp_names[0]][k]
+                # Create the empty point
+                z = np.zeros(f_point.shape[0])
+                if (len(f_point.shape) > 1):
+                    z = np.zeros((f_point.shape[0], f_point.shape[1]))
+                for n, v in zip(interp_names, interp_vals):
+                    z += self.interpolate_conditions[n][k] * v
+                #resamp = SerieDeformation().add_original(z, 65)
+
+                self._model.condition(k, z)
+                self.send_bach_series('/condition', k, z)
+
+        valid_points = list(filter(lambda x: interp_names[x] in self.interpolate_points.keys(), range(len(interp_names))))
+        interp_names = [interp_names[i] for i in valid_points]
+        interp_vals = [interp_vals[i] for i in valid_points]
+        if (self.interpolate_latent) and len(interp_names) > 0:
+            # Retrieve first point to infer size
+            f_point = self.interpolate_points[interp_names[0]]
+            # Create the empty point
+            z = np.zeros((f_point.shape[0], f_point.shape[1]))
+            target_shape = max([x.shape[1] for x in self.interpolate_points.values()])
+
+            for n, v in zip(interp_names, interp_vals):
+                if not n in self.interpolate_points.keys():
+                    continue
+                z += self.get_deformed_serie(self.interpolate_points[n], target_length=target_shape, axis=1).numpy() * v
+
+            #resamp = SerieDeformation().add_original(z, 65)
+            for s in range(z.shape[0]):
+                self.send_bach_series('/encode', s, z[s])
+            # Compute decoding
+            self.current_traj = z
+            result = self.decode('generate')
+            # Write result to file
+            #path = self.output_wav(result)
+            # Send the path to interface
+            #self.send('/decode', path)
+
+    def set_interpolation_mode(self, version, value):
+        if (version == 'latent'):
+            self.interpolate_latent = value
+        if (version == 'condition'):
+            self.interpolate_condition = value
 
     def condition(self, *args):
         cond_type = str(args[0])
